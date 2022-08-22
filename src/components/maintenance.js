d03e9f75485c061b61f6ac26bf2edbb0786a2371
saotome <jhahn618@gmail.com>
2022-08-22 01:22:55 -0400

    used .slice(0, -2) method to take off the last two characters in the finallist string in maintenance component

--- src/components/maintenance.js
+++ src/components/maintenance.js
@@ -1,16 +1,18 @@
 const Maintenance = (props) => {
   let listing = "";
+  let finallist = "";
   for (let i = 0; i < props.diseases.length; i++) {
     console.log(props.diseases);
 
     listing += props.diseases[i] + " | ";
+    finallist = listing.slice(0, -2);
   }
   return (
     <div className="maintenance-box plant-info-box">
       <h3>Maintenance Regimen: {props.maintenance}</h3>
       <h4>Pruning Period: {props.pruning}</h4>
 
-      <h4>Common Diseases: {listing} </h4>
+      <h4>Common Diseases: {finallist}</h4>
       <h4>Pesticide & Fungicide: {props.bugspray}</h4>
     </div>
   );
