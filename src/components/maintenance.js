0d15a45500d84f30311ef38a12bbbca5d3d19853
saotome <jhahn618@gmail.com>
2022-08-22 01:00:06 -0400

    added for loop to add spaces for diseases list

--- src/components/maintenance.js
+++ src/components/maintenance.js
@@ -1,9 +1,16 @@
 const Maintenance = (props) => {
+  let listing = "";
+  for (let i = 0; i < props.diseases.length; i++) {
+    console.log(props.diseases);
+
+    listing += props.diseases[i] + " | ";
+  }
   return (
     <div className="maintenance-box plant-info-box">
       <h3>Maintenance Regimen: {props.maintenance}</h3>
       <h4>Pruning Period: {props.pruning}</h4>
-      <h4>Common Diseases: {props.diseases}</h4>
+
+      <h4>Common Diseases: {listing} </h4>
       <h4>Pesticide & Fungicide: {props.bugspray}</h4>
     </div>
   );
